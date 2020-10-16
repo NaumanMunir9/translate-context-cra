7f3916517a54608535376982836c79051560e360
Nauman Munir <naumanmunir@gmail.com>
2020-10-16 18:50:42 +0500

    first pass - 16Oct20

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import ColorContext from "../contexts/ColorContext";
+import { LanguageStore } from "../contexts/LanguageContext";
+import LanguageSelector from "./LanguageSelector";
+import UserCreate from "./UserCreate";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="ui container">
+        <LanguageStore>
+          <LanguageSelector />
+          <ColorContext.Provider value="red">
+            <UserCreate />
+          </ColorContext.Provider>
+        </LanguageStore>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import ColorContext from "../contexts/ColorContext";
+import LanguageContext from "../contexts/LanguageContext";
+
+class Button extends Component {
+  renderSubmit = (language) => {
+    return language === "english" ? "Submit" : "Voorleggen";
+  };
+
+  renderButton = (color) => {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {({ language }) => this.renderSubmit(language)}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  };
+
+  render() {
+    return (
+      <ColorContext.Consumer>
+        {(color) => this.renderButton(color)}
+      </ColorContext.Consumer>
+    );
+  }
+}
+
+export default Button;
